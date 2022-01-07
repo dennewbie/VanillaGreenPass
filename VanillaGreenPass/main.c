49679d7b897b574d97ec523266f36e90a99befc1
Denny Caruso <41870472+dennewbie@users.noreply.github.com>
2022-01-07 17:18:27 +0100

    Initial Commit

---
+++ VanillaGreenPass/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  VanillaGreenPass
+//
+//  Created by Denny Caruso on 07/01/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
