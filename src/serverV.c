92e2693d5c3f45fede4944c95063c247e063fdf1
Denny Caruso <41870472+dennewbie@users.noreply.github.com>
2022-01-08 16:55:02 +0100

    added serverV (.c, .h, .conf)

---
+++ src/serverV.c
@@ -0,0 +1,8 @@
+//
+//  serverV.c
+//  VanillaGreenPass
+//
+//  Created by Denny Caruso on 08/01/22.
+//
+
+#include "serverV.h"
---
+++ src/serverV.h
@@ -0,0 +1,13 @@
+//
+//  serverV.h
+//  VanillaGreenPass
+//
+//  Created by Denny Caruso on 08/01/22.
+//
+
+#ifndef serverV_h
+#define serverV_h
+
+#include "GreenPassUtility.h"
+
+#endif /* serverV_h */
